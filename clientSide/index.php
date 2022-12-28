1c528539c9a705be3fa8d57c57c319a44266c346
Abdellatif-ait <ja_aiteur@esi.dz>
2022-12-28 21:48:06 +0100

    adding Card component

--- clientSide/index.php
+++ clientSide/index.php
@@ -3,7 +3,6 @@
     $layout = new layout();
     $layout->head("hi this is a test","this is a test");
     $layout->navbar();
-    // $layout->card('google','TEST',"description");
+    $layout->card(1,'google','TEST',"description description description descriptiondescription description descript iondescriptio ndescription description description vaaaaaa descriptiondescription description v ");
     $layout->footer();
-
 ?>
