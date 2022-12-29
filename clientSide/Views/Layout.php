9749a7dbf0cf7c5dcba7d12dd55941e30da51c8c
Abdellatif-ait <ja_aiteur@esi.dz>
2022-12-29 17:29:19 +0100

    Adding swiper and fixing card component

--- clientSide/Views/Layout.php
+++ clientSide/Views/Layout.php
@@ -13,12 +13,12 @@ class layout
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="pragma" content ="no cache" />
             <title><?php echo $title ?></title>
             <meta name="description" content=<?php echo $description ?> />
             <meta name="keywords" content=<?php echo self::KEYS ?> />
-            <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
             <link rel="stylesheet" href="style.css"> 
-
         </head>
     <?php
     }
@@ -108,14 +108,62 @@ class layout
     public function card($id, $url, $title, $description)
     {
     ?>
-        <a href="" class="card">
+        <div href="" class="card-container">
             <img src="Utils/test.jfif" alt="Logo" class="card-img">
             <div class="card-body">
-                <h5 class="card-title"><?php echo $title ?></h5>
-                <p class="card-text"><?php echo $description ?></p>
+                <h5 class="card-header"><?php echo $title ?></h5>
+                <p class="card-content"><?php echo $description ?></p>
             </div>
-        </a>
+            <a class="prm-btn">lire la suite</a>
+        </div>
 <?php
+    }
+
+    //Swiper
+    public function swipper(){
+        ?>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-pause="hover">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="2000">
+                        <div class="swiper-item">
+                            <img src="Utils/test.jfif" alt="testImg" class="swiper-item-img">
+                            <div class="swiper-item-body">
+                                <div class="swiper-item-text">
+                                    <h3>Title1</h3>
+                                    <p>Taco Pasta is made with pasta shells, ground beef, cheese, and a handful of spices and seasonings so you get a delicious dinner in minutes!</p>
+                                </div>
+                                <a href="" class="prm-btn">afficher la suite</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <div class="swiper-item">
+                            <img src="Utils/test.jfif" alt="testImg" class="swiper-item-img">
+                            <div class="swiper-item-body">
+                                <div class="swiper-item-text">
+                                    <h3>Title2</h3>
+                                    <p>Taco Pasta is made with pasta shells, ground beef, cheese, and a handful of spices and seasonings so you get a delicious dinner in minutes!</p>
+                                </div>
+                                <a href="" class="prm-btn">afficher la suite</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="swiper-item" data-bs-interval="2000">
+                            <img src="Utils/test.jfif" alt="testImg" class="swiper-item-img">
+                            <div class="swiper-item-body">
+                                <div class="swiper-item-text">
+                                    <h3>Title3</h3>
+                                    <p>Taco Pasta is made with pasta shells, ground beef, cheese, and a handful of spices and seasonings so you get a delicious dinner in minutes!</p>
+                                </div>
+                                <a href="" class="prm-btn">afficher la suite</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <?php
     }
 }
 ?>
--- clientSide/index.php
+++ clientSide/index.php
@@ -3,6 +3,7 @@
     $layout = new layout();
     $layout->head("hi this is a test","this is a test");
     $layout->navbar();
+    $layout->swipper();
     $layout->card(1,'google','TEST',"description description description descriptiondescription description descript iondescriptio ndescription description description vaaaaaa descriptiondescription description v ");
     $layout->footer();
 ?>
